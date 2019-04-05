26135cb43f119fe7bed0564863175f3ed887672d
David Sandberg <david.sandberg@soprasteria.com>
2019-04-05 15:50:13 +0200

    Added test message

--- spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/Message.java
+++ spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/Message.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+//This is a package
 package sample.atmosphere;
 
 import java.util.Date;
